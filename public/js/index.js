cb8153ccfdf79a176c6a58d9188e173d707949d1
Marcquin Taylor <Taylor@Quins-iMac.attlocal.net>
2018-01-09 13:50:32 -0500

    Add MustacheJS to render messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,25 +9,42 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  $('#messages').append(li);
-  console.log('New Message', message);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    time: formattedTime
+  });
+  $('#messages').append(html);
+  // const formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = $('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  // $('#messages').append(li);
+  // console.log('New Message', message);
 });
 
 socket.on('newLocationMessage', function(message) {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My Current Location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  var template = $('#location-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    time: formattedTime
+  });
+  $('#messages').append(html);
+  // const formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = $('<li></li>');
+  // var a = $('<a target="_blank">My Current Location</a>');
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // $('#messages').append(li);
 });
 
 $('#message-form').submit(function(ev) {
   ev.preventDefault();
   var text = $('#message').val();
+  if(text)
   socket.emit(
     'createMessage',
     {
